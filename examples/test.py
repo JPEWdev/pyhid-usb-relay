9c4b113d7462babf11f2f8dfafec53b75901ce11
Joshua Watt <JPEWhacker@gmail.com>
2022-09-02 10:47:00 -0500

    Update example script
    Updates the example script to make it clearer how it can and should be
    used

--- examples/test.py
+++ examples/test.py
@@ -1,22 +1,28 @@
+#! /usr/bin/env python3
 import pyhid_usb_relay
+import time
 
-def off(relay):
-    # If relay state is on (1) then we can turn it off.
-    if relay.state:
+if __name__ == "__main__":
+    relay = pyhid_usb_relay.find()
+
+    # Example of reading state and toggling relay #1
+    if relay.get_state(1):
         relay.toggle_state(1)
-    else:
-        print("The relay is already off")
+    time.sleep(3)
 
-def on(relay):
-    # If relay state is not on (1) then we can turn it on.
-    if not relay.state:
+    if not relay.get_state(1):
         relay.toggle_state(1)
-    else:
-        print("The relay is already on")
+    time.sleep(3)
 
-if __name__ == '__main__':
-    relay1 = pyhid_usb_relay.find()
-    # Turns on the relay
-    on(relay1) 
-    # You can also turn it off with this helper function
-    # off(relay1)
+    # You can also refer to relays by index
+    if relay[1]:
+        relay[1] = False
+    time.sleep(3)
+
+    if not relay[1]:
+        relay[1] = True
+    time.sleep(3)
+
+    # If you have relay aliases defined in your config file, you can also refer
+    # to them in place of the index:
+    relay["raspberrypi4"] = not relay["raspberrypi4"]
